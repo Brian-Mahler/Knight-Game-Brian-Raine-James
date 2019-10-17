93f5b7224600363d0c4712f2857d9636827ff87b
Raine <raine.johnson@digipen.edu>
2019-10-17 09:22:17 -0700

    Added Code Notes and Modded Art

--- FINAL GAME/Flat Platformer Template/Scripts/DamageOnCollide.cs
+++ FINAL GAME/Flat Platformer Template/Scripts/DamageOnCollide.cs
@@ -18,6 +18,35 @@ public class DamageOnCollide : MonoBehaviour
     public UnityEvent DamageFunctions;
     //functions to run when object is set to destroy itself
     public UnityEvent DestroyFunctions;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Health otherHealth = collision.gameObject.GetComponent<Health>();
+        if (otherHealth != null)
+        {
+            otherHealth.Damage(DamageAmount);
+            DamageFunctions.Invoke();
+        }
+        if (DestroyOnCollide)
+        {
+            DestroyFunctions.Invoke();
+            Destroy(gameObject);
+        }
+    }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Health otherHealth = collision.gameObject.GetComponent<Health>();
+        if (otherHealth != null)
+        {
+            otherHealth.Damage(DamageAmount);
+            DamageFunctions.Invoke();
+        }
+        if (DestroyOnCollide)
+        {
+            DestroyFunctions.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+
 }
